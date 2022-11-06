dcefc1e0341e023aa09700756b87e0d818e79b3d
Antonio Carlos Ramos <enigmacrs@hotmail.com>
2022-11-06 17:33:55 -0300

    Create Ola Mundo.php
    Primeiro repositório da aula Github

---
+++ Ola Mundo.php
@@ -0,0 +1,14 @@
+﻿<!DOCTUYPE html>
+<html>
+<head>
+<meta charset="UTF-8"/>
+<title>  Olá Mundo  </title>
+</head>
+<body>
+	<h1>Testando o PHP</h1>
+<!-- super tag - tag que inclui outras linguagens*/ -->
+	<?php
+		echo "<h2>Olá Mundo</h2>";
+	?>
+</body>
+</html>
